7121711477fd05b12332c23cf5bb926d2f95723e
Yunoo <fiery.izi@gmail.com>
2015-12-18 14:37:40 +0100

    Add check for running only one daash instance

--- ImageShare/Program.cs
+++ ImageShare/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace daash
@@ -8,11 +11,36 @@ namespace daash
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            using (var mainForm = new MainForm())
+            Mutex SingleInstanceMutex;
+            try
             {
-                Application.Run(mainForm);
+                SingleInstanceMutex = Mutex.OpenExisting("daash");
+            }
+            catch (Exception)
+            {
+                SingleInstanceMutex = new Mutex(false, "daash");
+            }
+
+            try
+            {
+                if (SingleInstanceMutex.WaitOne(0, false))
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new MainForm());
+                }
+                else
+                {
+                    MessageBox.Show("It is only possible to run one instance of daash", "daash is already running", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            finally
+            {
+                if (SingleInstanceMutex != null)
+                {
+                    SingleInstanceMutex.Close();
+                    SingleInstanceMutex = null;
+                }
             }
         }
     }
